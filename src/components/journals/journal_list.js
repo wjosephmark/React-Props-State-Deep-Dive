97350d157f65981528aa398082a71474b4b31774
wjosephmark <wjosephmark@gmail.com>
2020-04-06 14:57:13 -0600

    Added buttons

--- src/components/journals/journal_list.js
+++ src/components/journals/journal_list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { JournalEntry } from "./journal_entry"
 
 const rawJournalData = [
     {title: 'Post One', content: 'Post content', status: 'draft'},
@@ -7,22 +8,53 @@ const rawJournalData = [
     {title: 'Post Four', content: 'Post content', status: 'Published'}
 ]
 
+
 export default class JournalList extends Component {
     constructor(props) {
         super()
 
         this.state = {
-            rawJournalData: rawJournalData,
+            journalData: rawJournalData,
             greeting: "Hi there",
             isOpen: true
         }
     }
 
+    clearEntries = () => {
+        this.setState({ journalData: [], isOpen: false })
+    }
+    ShowAllEntries = () => {
+        this.setState({ journalData: rawJournalData, isOpen: true })
+    }
+    ToggleEntries = () => {
+        if (this.state.isOpen) {
+            this.setState({ journalData: [], isOpen: false })
+        } else {
+            this.setState({ journalData: rawJournalData, isOpen: true })
+        }
+    }
+
     render() {
+        const journalEntries = this.state.journalData.map(journalEntry => {
+            return (
+                <div key={journalEntry.title}>
+                    <JournalEntry
+                        title={journalEntry.title}
+                        content={journalEntry.content}
+
+                    />
+                </div>
+            )
+        })
+
         return (
             <div>
                 <h2>{this.props.heading}</h2>
-            </div>
+                {journalEntries}
+                <button onClick={this.clearEntries}> Clear Journal Entries </button>
+                <button onClick={this.ShowAllEntries}> Show All Entries </button>
+                <button onClick={this.ToggleEntries}> Toggle Entries </button>
+            </div> 
         )
     }
 }
